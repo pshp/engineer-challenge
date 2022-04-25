9ed3a766bb410d3752b0082bcfd6aeef6524c89d
Peter hp <phirschparker@gmail.com>
2022-04-25 04:03:08 +0200

    feat: server does not explicity listen to a port when running tests

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -49,6 +49,10 @@ app.get('/', (req, res) => {
   res.send('Server is up and running 🚀')
 })
 
-app.listen(port, () => {
-  console.log(`🚀  Server ready at ${port}`);
-});
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(port, () => {
+    console.log(`🚀  Server ready at ${port}`);
+  });
+}
+
+export default app
