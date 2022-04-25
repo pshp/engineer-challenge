f6860e91551e439dc7a478628f0795f2544a140c
Peter hp <phirschparker@gmail.com>
2022-04-25 22:39:16 +0200

    fix: where condition fixed, search query now working. All tests passing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,35 +1,39 @@
-import express, { Request, Response } from 'express';
-import { PrismaClient, Prisma } from '@prisma/client';
+import express, { Request, Response } from "express";
+import { PrismaClient, Prisma } from "@prisma/client";
 const app = express();
 const port = 4000;
 const prisma = new PrismaClient();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/policies', async (req: Request, res: Response) => {
+app.get("/policies", async (req: Request, res: Response) => {
   const { search } = req.query;
 
   const searchQuery: Prisma.PolicyWhereInput = search
     ? {
-      OR: [
-        { provider: { contains: search as string, mode: 'insensitive' } },
-        { customer: { firstName: { contains: search as string, mode: 'insensitive' } } },
-        { customer: { lastName: { contains: search as string, mode: 'insensitive' } } }
-      ],
-    }
+        OR: [
+          { provider: { contains: search as string, mode: "insensitive" } },
+          {
+            customer: {
+              firstName: { contains: search as string, mode: "insensitive" },
+            },
+          },
+          {
+            customer: {
+              lastName: { contains: search as string, mode: "insensitive" },
+            },
+          },
+        ],
+      }
     : {};
 
-    const statusFilter: Prisma.PolicyWhereInput = {
-      OR: [
-        { status: 'ACTIVE' },
-        { status: 'PENDING' }
-      ],
-    };
+  const statusFilter: Prisma.PolicyWhereInput = {
+    OR: [{ status: "ACTIVE" }, { status: "PENDING" }],
+  };
 
   const policies = await prisma.policy.findMany({
     where: {
-      ...searchQuery,
-      ...statusFilter
+      AND: [{ ...searchQuery }, { ...statusFilter }],
     },
     select: {
       id: true,
@@ -43,23 +47,23 @@ app.get('/policies', async (req: Request, res: Response) => {
           id: true,
           firstName: true,
           lastName: true,
-          dateOfBirth: true
-        }
-      }
-    }
-  })
+          dateOfBirth: true,
+        },
+      },
+    },
+  });
 
   res.json(policies);
-})
+});
 
-app.get('/', (req, res) => {
-  res.send('Server is up and running 🚀')
-})
+app.get("/", (req, res) => {
+  res.send("Server is up and running 🚀");
+});
 
-if (process.env.NODE_ENV !== 'test') {
+if (process.env.NODE_ENV !== "test") {
   app.listen(port, () => {
     console.log(`🚀  Server ready at ${port}`);
   });
 }
 
-export default app
+export default app;
