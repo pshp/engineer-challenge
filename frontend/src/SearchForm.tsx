b33bb46f3a6c686e055b8e7dfbee4f1dfcc6b0b3
Peter hp <phirschparker@gmail.com>
2022-04-29 20:54:45 +0100

    feat: search form componant rendering

--- frontend/src/SearchForm.tsx
+++ frontend/src/SearchForm.tsx
@@ -1,1 +1,11 @@
-export {}
+const SearchForm = () => {
+
+  return (
+    <form className="sf-container">
+      <input className="wmx5 mt8" placeholder="search" />
+      <button className="wmn3 mt8" placeholder="Click me" />
+    </form>
+  )
+}
+
+export default SearchForm;
